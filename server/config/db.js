8c43ac9d86070cc23b1f8ce7f2265eae59e37687
priyanshu-verma600 <ipriyanshu7667@gmail.com>
2026-07-04 12:25:04 +0530

    Remove console log of MongoDB URI
    Remove logging of MongoDB URI for security reasons.

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGODB_URI);
-
         mongoose.connection.on("connected", () => {
             console.log("Database connected");
         });
